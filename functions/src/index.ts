279750bd67a46f306706aedd02b8c9470ce6a381
lafken <lafkencossi35@gmail.com>
2022-09-11 15:18:04 -0300

    primer commit del Experimento con angular

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,7 +9,7 @@ app.use(cors({ origin: true }));
 
 
 app.get('/', (req, res) => {
- res.send('hola WORLD!');
+ res.send('hola WORLD!s');
 });
 
 app.post('/', (req, res) => {
@@ -26,20 +26,35 @@ app.post('/', (req, res) => {
   return res.send(data).end();
  })
 
+ app.get('/horas',  (req,res) => {
+  //  res.send(Horario)
+  const horario = currentTime();
+  res.send(horario)
+});
+ 
 exports.app = functions.https.onRequest(app)
 
- export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("que onda bro ya es la 2 ves que haces eso acordate de git pedaso de B***** att:lafken del pasado =3");
-  });
+//  export const helloWorld = functions.https.onRequest((request, response) => {
+//     functions.logger.info("Hello logs!", {structuredData: true});
+//     response.send("que onda bro ya es la 2 ves que haces eso acordate de git "esta parte a sido borada por culpa de la generacion de cristal" att:lafken del pasado =3");
+//   });
+
+function currentTime() {
+  const current = new Date;
+return {
+ // Datos:new Date(),
+  iso: current.toISOString(),
+  epoch: current.getTime()
+}
+}
 
 
 
 
-  
+
 //priemr intento funcional
-let horario = new Date();
-console.log(horario.getHours(),horario.getMinutes(),horario.getSeconds())
+// let horario = new Date();
+// console.log(horario.getHours(),horario.getMinutes(),horario.getSeconds())
 
 
 //BASURA QUE NO FUNCIONA--------------------------------------
